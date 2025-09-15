0b123025f46b0bcd587bb6b6dd2d729b290a92ec
Docile123 <imbereyemasoo@gmail.com>
2025-09-16 01:17:50 +0200

    feat:success stories

--- src/common-components/Navbar.jsx
+++ src/common-components/Navbar.jsx
@@ -46,6 +46,11 @@ function Navbar() {
             </NavLink>
           </li>
           <li className="text-gray-700 dark:text-gray-200 relative group">
+            <NavLink to="/success-stories" className="group-hover:text-gray-500 dark:group-hover:text-gray-300 transition duration-300 ease-in-out">success Stories
+              <div className="absolute w-0 bg-green-400  left-0 -bottom-2 h-1.5 group-hover:w-full transition-all duration-300 ease-in-out"></div>
+            </NavLink>
+          </li>
+          <li className="text-gray-700 dark:text-gray-200 relative group">
             <NavLink to="/ai-chatbot" className="group-hover:text-gray-500 dark:group-hover:text-gray-300 transition duration-300 ease-in-out">Ask our AI
               <div className="absolute w-0 bg-green-400  left-0 -bottom-2 h-1.5 group-hover:w-full transition-all duration-300 ease-in-out"></div>
             </NavLink>
@@ -85,6 +90,12 @@ function Navbar() {
         >
           <ul className="m-auto w-11/12 max-w-sm flex flex-col gap-y-4 bg-gray-100 dark:bg-gray-900 rounded-lg shadow-lg px-4 py-6 transition-all duration-300 transform scale-95 opacity-0 animate-navbarIn">
             <li className="text-gray-700 dark:text-gray-200 relative group">
+              <NavLink to="/tvetmatters" className="group-hover:text-gray-500 dark:group-hover:text-gray-300 transition duration-300 ease-in-out" onClick={() => setMenuOpen(false)}>
+                Why TVET
+                <div className="absolute w-0 bg-gray-400 dark:bg-gray-700 left-0 -bottom-2 h-1.5 group-hover:w-full transition-all duration-300 ease-in-out"></div>
+              </NavLink>
+            </li>
+             <li className="text-gray-700 dark:text-gray-200 relative group">
               <NavLink to="/tvetmatters" className="group-hover:text-gray-500 dark:group-hover:text-gray-300 transition duration-300 ease-in-out" onClick={() => setMenuOpen(false)}>
                 Why TVET
                 <div className="absolute w-0 bg-gray-400 dark:bg-gray-700 left-0 -bottom-2 h-1.5 group-hover:w-full transition-all duration-300 ease-in-out"></div>
