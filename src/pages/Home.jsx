4358e55b757f8b0617dcd458dc1df934bf653c25
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2025-09-18 06:51:05 +0200

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
   }, []);
   return (
     <div className=" min-h-screen bg-[#E4F8E2] dark:bg-gray-900 transition-colors duration-300">
-      <div className="absolute w-full min-h-screen flex-grow h-[750px] overflow-hidden flex flex-col justify-center">
+      <div className="absolute w-full min-h-screen  flex-grow h-[750px] overflow-hidden hidden sm:hidden lg:flex flex-col justify-center">
         {/* Video Background with Low Brightness Filter */}
         <video
           autoPlay
@@ -40,6 +40,33 @@ const Home = () => {
           }}
         >
           <source src="/video/bg-vid.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Primary Dark Overlay for depth */}
+        {/* <div className="absolute inset-0 bg-black/40 dark:bg-black/60"></div> */}
+
+        {/* Gradient Overlay for smooth blending */}
+        <div className="absolute inset-0 bg-gradient-to-b from-black/100 via-transparent to-[#E4F8E2]/0 dark:to-gray-900/90"></div>
+
+        {/* Subtle vignette effect */}
+        <div className="absolute inset-0 bg-gradient-radial from-transparent via-transparent to-black/20"></div>
+
+        </div>
+        
+        <div className="absolute w-full min-h-screen  flex-grow h-[750px] overflow-hidden flex sm:flex lg:hidden md:hidden flex-col justify-center">
+        {/* Video Background with Low Brightness Filter */}
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="hero-video absolute inset-0 w-full mx-auto h-full object-cover opacity-60 brightness-[12] mt-[-30px] contrast-110 saturate-75 blur-[0.5px]"
+          style={{
+            filter: 'brightness(0.83) contrast(1.1) saturate(1) blur(0.5px)'
+          }}
+        >
+          <source src="/video/bg-vid2.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
